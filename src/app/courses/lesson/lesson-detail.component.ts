22831090bc5dc01f9a5fb1d13fd02a838168895e
unknown <a.cina@procreatech.com>
2024-11-20 12:41:04 +0100

    19. Understanding Route Snapshots vs Route Observables - When to Use Each and Why

--- src/app/courses/lesson/lesson-detail.component.ts
+++ src/app/courses/lesson/lesson-detail.component.ts
@@ -11,16 +11,29 @@ import { map } from "rxjs/operators";
 })
 export class LessonDetailComponent implements OnInit {
   lesson$: Observable<LessonDetail>;
-  lesson: LessonDetail;
+  // lesson: LessonDetail;
 
   // per navigare ho bisogno del Router, quindi lo inietto
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
-    // this.route.data.subscribe((val) => console.log(val.lesson));
-
     // console.log(this.route.snapshot.data.lesson);
-    this.lesson = this.route.snapshot.data["lesson"];
+
+    // ho problemi con questo modo di assegnazione dati alla proprietà lesson
+    // this.lesson = this.route.snapshot.data["lesson"];
+
+    // il componente viene instanziato una sola volta, quando chiamo i metodi next e previous io devo aggiornarne il contenuto
+    // prendendo i dati con route.snapshot è come se io avessi un'istantanea dei dati e questi rimarranno sempre gli stessi, cioè la lesson rimane sempre quella salvata al momento della creazione del componente
+    // questo succede ogni volta che con la navigazione rivado sempre allo stesso componente, da un componente navigo verso se stesso, non creo una nuova instance del component
+    // per far aggiornare i dati posso utizzare un observable, l'observable data dell'oggetto route
+
+    // this.route.data.subscribe((val) => (this.lesson = val.lesson));
+
+    // se usassi il subscribe avrei un problema per passarlo all'html ed utilizzare il pipe async, perchè questo fa la sottoscrizione lui, quindi utilizzo il pipe ed il metodo map in modo da avere un observable che restituisce un oggetto LessonDetail
+    this.lesson$ = this.route.data.pipe(map((val) => val.lesson));
+
+    // il router resolver dà come risultato un observable che emette un valore
+    // lo snapshot è il metodo più veloce e conveniente per prendere il primo valore emesso quando il componente viene instanziato la prima volta con quella rotta e quindi ha il primo risultato emesso dal resolver
   }
 
   previous(lesson: LessonDetail) {
